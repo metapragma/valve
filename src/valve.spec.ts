fd715db9edfcb09fc288ac8b7f377caad895941a
l5x <189198+l5x@users.noreply.github.com>
2018-08-03 16:07:21 +0000

    feat: two more tests

--- src/valve.spec.ts
+++ src/valve.spec.ts
@@ -1,5 +1,5 @@
 // import { fromIterable, map, reduce, through, valve } from './index'
-import { collect, count, createThrough, error, fromIterable, map, valve } from './index'
+import { collect, count, createThrough, error, fromIterable, map, reduce, valve } from './index'
 import { ValveActionType, ValveType } from './types'
 import { spy } from 'sinon'
 
@@ -145,44 +145,44 @@ describe('pull', () => {
     )
   })
 
-  // it('wrap pull streams into stream', done => {
-  //   valve(
-  //     fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-  //     map(e => e * e),
-  //     through(),
-  //     reduce({
-  //       iteratee(acc, data) {
-  //         return acc + data
-  //       },
-  //       onData(action) {
-  //         assert.equal(action.payload, 385)
-  //         done()
-  //       }
-  //     })
-  //   )
-  // })
-  //
-  // it('turn pull(through,...) -> Through', done => {
-  //   valve(
-  //     fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-  //     valve(
-  //       map<number, number>(e => {
-  //         return e * e
-  //       }),
-  //       through<number>()
-  //     ),
-  //     reduce({
-  //       iteratee(acc, data) {
-  //         return acc + data
-  //       },
-  //       onData(action) {
-  //         assert.equal(action.payload, 385)
-  //         done()
-  //       }
-  //     })
-  //   )
-  // })
-  //
+  it('wrap pull streams into stream', done => {
+    valve(
+      fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+      map(e => e * e),
+      createThrough(),
+      reduce({
+        iteratee(acc, data) {
+          return acc + data
+        },
+        onData(data) {
+          assert.equal(data, 385)
+          done()
+        }
+      })
+    )
+  })
+
+  it('turn pull(through,...) -> Through', done => {
+    valve(
+      fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+      valve(
+        map<number, number>(e => {
+          return e * e
+        }),
+        createThrough<number, number>()
+      ),
+      reduce({
+        iteratee(acc, data) {
+          return acc + data
+        },
+        onData(data) {
+          assert.equal(data, 385)
+          done()
+        }
+      })
+    )
+  })
+
   // it('writable pull() should throw when called twice', () => {
   //   const stream = valve<number, number>(
   //     map((e: number) => {
